fb568cc38355acb26ff17dbbb117714a6c31cf4a
Mattias Rost <rrostt@gmail.com>
2023-09-18 13:04:23 +0000

    Adds CORS and routes.

--- app.py
+++ app.py
@@ -1,7 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={
+    r"/*": {
+        "methods": ["GET", "PUT", "OPTIONS", "POST"],  # Allow OPTIONS for preflight
+        "allow_headers": ["Content-Type", "Authorization"],
+        'origins': '*'
+    }
+})
+todos = [
+    {
+        "text": "Ta bort mig",
+        "done": False,
+    }
+]
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html", title="Hello")
+    return "Hello world"
+
+@app.route("/todos", methods=["GET"])
+def getTodos():
+    return todos
+
+@app.route("/todos", methods=["POST"])
+def addTodo():
+    todos.append(request.json)
+    return True
+
+@app.route("/todos/<int:index>", methods=["PUT"])
+def updateTodo(index):
+    todos[index] = request.json
+
+@app.route("/todos/<int:index>", methods=["GET"])
+def getTodo(index):
+    return todos[index]
